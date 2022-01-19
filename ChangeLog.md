c0266515d2345fc313b97170129b3568030fe10a
Kenny Flores <kennyjesus@protonmail.com>
2022-01-19 11:40:14 +0100

    Add Stack project

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for TFG-kenflohua
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,6 @@
+module Lib
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "Prueba de que funciona todo"
