497587c2f73f5c4b6f4198eb1fd30c5d493e034d
Kenny Flores <kennyjesus@protonmail.com>
2022-03-16 12:52:46 +0100

    Fix histogram and Add BlackAndWhite in Accelerate

--- src/Accelerate.hs
+++ src/Accelerate.hs
@@ -15,27 +15,54 @@ import Data.Array.Accelerate.Interpreter as I
 --}
 
 
-gHistogram :: Acc (Array DIM2 (Pixel8, Pixel8, Pixel8))-> (Acc (Vector Int), Acc (Vector Int), Acc (Vector Int))
+gHistogram :: Acc (Matrix (Pixel8, Pixel8, Pixel8))-> (Acc (Vector Int), Acc (Vector Int), Acc (Vector Int))
 gHistogram arr =
     let (r,g,b) = A.unzip3 arr -- (Acc (Array sh a), Acc (Array sh b), Acc (Array sh c)) 
-    in (histogram $ flatten $ promoteInt r, histogram $ flatten $ promoteInt g, histogram $ flatten $ promoteInt b)
+    in (compute $ histogram $ flatten $ promoteInt r, compute $ histogram $ flatten $ promoteInt g,compute $ histogram $ flatten $ promoteInt b)
 
 histogram :: Acc (Vector Int) -> Acc (Vector Int)
-histogram xs =
-    let zeros = fill (constant (Z:.10)) 0
-        ones  = fill (shape xs)         1
-        in permute (+) zeros (\ix -> Just_ (I1 (xs!ix))) ones
+histogram img =
+    let zeros = fill (constant (Z:.256)) 0
+        ones  = fill (shape img)         1
+        in permute (+) zeros (\ind -> Just_ (I1 (img!ind))) ones
 
 
-promoteInt :: Acc (Array DIM2 Pixel8) -> Acc (Array DIM2 Int)
+promoteInt :: Acc (Matrix Pixel8) -> Acc (Matrix Int)
 promoteInt = A.map A.fromIntegral
+
+
+{- 
+  ____     ___    __        __
+ | __ )   ( _ )   \ \      / /
+ |  _ \   / _ \/\  \ \ /\ / / 
+ | |_) | | (_>  <   \ V  V /  
+ |____/   \___/\/    \_/\_/   
+                              
+FORMULA PARA SACAR LA LUMINOSIDAD
+    Y' = 0.2989 R + 0.5870 G + 0.1140 B 
+    (https://en.wikipedia.org/wiki/Grayscale)
+
+-}
+
+grayScale :: Acc (Matrix (Pixel8,Pixel8,Pixel8)) -> Acc (Matrix Float)
+grayScale = A.map (\ pix ->
+    let (r,g,b) = unlift pix
+        r'      =  A.fromIntegral r / 255
+        g'      = A.fromIntegral g / 255
+        b'      = A.fromIntegral b / 255
+        in r' * 0.3 + g' * 0.59 + b' * 0.11)
+
 
 test :: IO ()
 test  =  do
     putStrLn "Iniciando Test Fichero Accelerate.hs"
+    {--Leyendo imágenes --}
     img <- readImageAcc "saitama.png"
-    -- print img
-    print $ gHistogram (use img)
-    
 
+    {--Creación Histograma--}
+    -- let (r,g,b) = gHistogram (use img)
+    -- print (run r, run g , run b)
+
+    {--Blanco y Negro--}
+    print $ run $ grayScale (use img)
     putStrLn "ASD"
